14c78b72c51e3b3369351b77547105dd2a4d6ad2
liuwei <liu.wei91@zte.com.cn>
2018-11-19 17:32:18 +0800

    feature : basic implementation

--- projects/components/communicate/testcases/testcase-eureka/eureka-client/src/java/com/zte/mw/components/communicate/restful/client/RestfulSmartLinkClient.java
+++ projects/components/communicate/testcases/testcase-eureka/eureka-client/src/java/com/zte/mw/components/communicate/restful/client/RestfulSmartLinkClient.java
@@ -8,10 +8,18 @@
 
 package com.zte.mw.components.communicate.restful.client;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriBuilder;
+
+import org.glassfish.jersey.client.ClientConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import com.zte.mw.components.communicate.restful.JsonUtil;
 import com.zte.mw.components.communicate.smartlink.LinkRepository;
 import com.zte.mw.components.communicate.smartlink.model.Address;
 import com.zte.mw.components.communicate.smartlink.model.Link;
@@ -29,7 +37,28 @@ public class RestfulSmartLinkClient {
     public static void main(String[] args) throws Exception {
         TestBuilder.setUp();
         LinkRepository.add(new Link("smart-link client", "address sync", "smart-link server"));
-        addressBook("smart-link client").add("smart-link server", new Address() {
+        addressBook("smart-link client").add("smart-link server", new Address<Request<Response>, Response>() {
+            @Override
+            public Response on(final Request<Response> msg) {
+                ClientConfig config = new ClientConfig();
+                config.register(JsonProvider.class);
+                Client client = ClientBuilder.newClient(config);
+                WebTarget target = client.target(UriBuilder.fromUri("http://localhost:8077").build());
+
+                try {
+                    return target.path("on").queryParam("name", JsonUtil.toString(msg)).request().accept(
+                            MediaType.APPLICATION_JSON).get(msg.getRespClass());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                // TODO: 11/19/18 find server url via eureka
+                // TODO: 11/19/18 send msg according to TestSendingJersey
+                //                WebResource resource = Client.create().resource(url);
+                //                return resource.get(msg.getRespClass());
+                return null;
+            }
+
             @Override
             public void bind(final Service service) {
 
@@ -37,13 +66,6 @@ public class RestfulSmartLinkClient {
 
             @Override
             public Address publish(final SmartLinkNode smartLinkNode) {
-                return null;
-            }
-
-            @Override
-            public Response on(final Request msg) {
-//                WebResource resource = Client.create().resource(url);
-//                return resource.get(msg.getRespClass());
                 return null;
             }
         });
--- projects/components/communicate/testcases/testcase-eureka/eureka-client/src/java/com/zte/mw/components/communicate/restful/client/TestSendingJersey.java
+++ projects/components/communicate/testcases/testcase-eureka/eureka-client/src/java/com/zte/mw/components/communicate/restful/client/TestSendingJersey.java
@@ -24,42 +24,11 @@ public class TestSendingJersey {
         TestBuilder.setUp();
         ClientConfig config = new ClientConfig();
         config.register(JsonProvider.class);
-
         Client client = ClientBuilder.newClient(config);
-
         WebTarget target = client.target(UriBuilder.fromUri("http://localhost:8077").build());
-
-        //        String response = target.path("on").queryParam("msg", new RegisterMsg(new AddressBook() {
-        //            @Override
-        //            public String toString() {
-        //                return "fake addressBook";
-        //            }
-        //        }, new Address() {
-        //            @Override
-        //            public void bind(final Service service) {
-        //
-        //            }
-        //
-        //            @Override
-        //            public Address publish(final SmartLinkNode smartLinkNode) {
-        //                return null;
-        //            }
-        //
-        //            @Override
-        //            public com.zte.mw.components.communicate.smartlink.model.Response on(
-        //                    final Request msg) {
-        //                return null;
-        //            }
-        //
-        //            @Override
-        //            public String toString() {
-        //                return "fake client address";
-        //            }
-        //        })).request().accept(MediaType.TEXT_PLAIN).get(Response.class).toString();
 
         String response = target.path("hello").queryParam("name", "Liuwei").request().accept(
                 MediaType.APPLICATION_JSON).get(Hello.class).toString();
-
         System.out.println(response);
     }
 }
