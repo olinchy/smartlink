b7939e7ee0e50121cc420a3d18a1ebb6a293bb27
liuwei <liu.wei91@zte.com.cn>
2018-11-21 17:30:05 +0800

    feature : finish injection test

--- projects/components/communicate/testcases/testcase-eureka/eureka-client/src/java/com/zte/mw/components/communicate/restful/client/RestfulSmartLinkClient.java
+++ projects/components/communicate/testcases/testcase-eureka/eureka-client/src/java/com/zte/mw/components/communicate/restful/client/RestfulSmartLinkClient.java
@@ -12,11 +12,11 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.UriBuilder;
 
 import org.glassfish.jersey.client.ClientConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import com.zte.mw.components.communicate.restful.JsonUtil;
@@ -34,8 +34,7 @@ import static com.zte.mw.components.communicate.smartlink.addressBook.AddressBoo
 @EnableDiscoveryClient
 @SpringBootApplication
 public class RestfulSmartLinkClient {
-    public static void main(String[] args) throws Exception {
-        TestBuilder.setUp();
+    public RestfulSmartLinkClient(final DiscoveryClient discoveryClient) {
         LinkRepository.add(new Link("smart-link client", "address sync", "smart-link server"));
         addressBook("smart-link client").add("smart-link server", new Address<Request<Response>, Response>() {
             @Override
@@ -43,20 +42,15 @@ public class RestfulSmartLinkClient {
                 ClientConfig config = new ClientConfig();
                 config.register(JsonProvider.class);
                 Client client = ClientBuilder.newClient(config);
-                WebTarget target = client.target(UriBuilder.fromUri("http://localhost:8077").build());
-
+                WebTarget target = client.target(discoveryClient.getInstances("smartlink-eureka-server").get(
+                        0).getUri());
                 try {
                     return target.path("on").queryParam("name", JsonUtil.toString(msg)).request().accept(
                             MediaType.APPLICATION_JSON).get(msg.getRespClass());
                 } catch (Exception e) {
                     e.printStackTrace();
+                    return null;
                 }
-
-                // TODO: 11/19/18 find server url via eureka
-                // TODO: 11/19/18 send msg according to TestSendingJersey
-                //                WebResource resource = Client.create().resource(url);
-                //                return resource.get(msg.getRespClass());
-                return null;
             }
 
             @Override
@@ -69,7 +63,10 @@ public class RestfulSmartLinkClient {
                 return null;
             }
         });
+    }
 
+    public static void main(String[] args) throws Exception {
+        TestBuilder.setUp();
         SpringApplication.run(RestfulSmartLinkClient.class, args);
     }
 }
